e2551086e8045bf321508cd2921d01040d2c923a
liusp86 <514773639@qq.com>
2015-03-11 22:44:45 +0800

    new

---
+++ src/com/imooc/guessmusic/ui/MainActivity.java
@@ -0,0 +1,70 @@
+package com.imooc.guessmusic.ui;
+
+import com.imooc.guessmusic.R;
+
+import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	//唱片相关动画
+	private Animation mPanAnim;
+	private LinearInterpolator mPanLin;
+	
+	//拨杆相关动画
+	private Animation mBarInAnim;
+	private LinearInterpolator mBarInLin;
+	
+	private Animation mBarOutAnim;
+	private LinearInterpolator mBarOutLin;
+	
+	//Play 按键事件
+	private ImageButton mBtnPlayStart;
+
+	private ImageView mViewPan;
+	private ImageView mViewPanBar;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mViewPan = (ImageView) findViewById(R.id.imageView_disc);
+		mViewPanBar = (ImageView) findViewById(R.id.imageView_bar);
+		
+		//初始化动画
+		mPanAnim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+		mPanLin = new LinearInterpolator();
+		mPanAnim.setInterpolator(mPanLin);
+		
+		mBarInAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_45);
+		mBarInLin = new LinearInterpolator();
+		mBarInAnim.setInterpolator(mBarInLin);
+		
+		mBarOutAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_d_45);
+		mBarOutLin = new LinearInterpolator();
+		mBarOutAnim.setInterpolator(mBarOutLin);
+		
+		mBtnPlayStart = (ImageButton) findViewById(R.id.btn_play_start);
+		mBtnPlayStart.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_LONG).show();
+			}
+		});
+	}
+
+	
+}
